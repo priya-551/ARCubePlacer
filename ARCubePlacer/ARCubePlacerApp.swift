a801b214053c7e07f48bad7a73f02b9027c19cdc
Priya <priyatetala@gmail.com>
2025-07-31 10:22:47 +0530

    Initial Commit

---
+++ ARCubePlacer/ARCubePlacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARCubePlacerApp.swift
+//  ARCubePlacer
+//
+//  Created by Priya reddy on 31/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ARCubePlacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
